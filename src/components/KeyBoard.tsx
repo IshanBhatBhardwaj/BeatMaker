12f8b1574758f9e4bcb7becc809c74e67a2fe114
IshanBhatBhardwaj <ishanbhatbhardwaj@icloud.com>
2025-02-02 19:25:08 -0500

    added layers

--- src/components/KeyBoard.tsx
+++ src/components/KeyBoard.tsx
@@ -12,6 +12,11 @@ interface Beat {
 const KeyBoard = () => {
     const soundMap = useSoundMapContext();
     const [beats, setBeat] = useState<Beat[]>([])
+    const [layers, setLayers] = useState<Beat[][]>([])
+    const [looping, setLooping] = useState<boolean>(false)
+    const [bpm, setBpm] = useState<number>(100)
+    const [bars, setBars] = useState<number>(8)
+
 
     useEffect(() => {
         window.addEventListener("keydown", handleKeyPress);
@@ -23,7 +28,6 @@ const KeyBoard = () => {
     }
 
     const handleKeyPress = (e: KeyboardEvent) => {
-        console.log("called")
         const keyPressed = e.key;
 
         const soundMapValue = soundMap.soundMap.get(keyPressed);
@@ -35,30 +39,56 @@ const KeyBoard = () => {
         playHowlSound(soundMapValue);
    }
 
+   const testMe = () => {
+    setLayers((prevLayer) => [...prevLayer, beats])
+    setBeat([]);
+   }
 
-    const playLoop = async () => {
-       if (beats.length === 0) return;
+   //BUG FIX: Each beat should NOT start right away. We have fill each Beat[] with an "empty" beat that just has a timeStamp. That way we can play a pause if the beat starts on a 2 and not a 1
+   //Get the structure to work right. We should be able to add beats easily!! And while the loop is playing!!!
+    const playLayer = async (layer: Beat[]) => {
+
+        let timePerBaMs = (60 * bars / bpm) * 1000;
+        console.log(timePerBaMs);
+       if (layer.length === 0) return;
    
-       let prevTime = beats[0].timeStamp;
-        for (const beat of beats) {
-         const delay = beat.timeStamp - prevTime;
-         prevTime = beat.timeStamp;
-
-         await timeout(delay);
-         console.log(delay)
-
-         const value = soundMap.soundMap.get(beat.key);
-
-         if (value === undefined) {return};
-
-         playHowlSound(value);
-       };
+       let prevTime = layer[0].timeStamp;
+       while (looping) {
+        let curTime = Date.now();
+        for (const beat of layer) {
+            const delay = beat.timeStamp - prevTime;
+            prevTime = beat.timeStamp;
+   
+            await timeout(delay);
+   
+            const value = soundMap.soundMap.get(beat.key);
+   
+            if (value === undefined) {return};
+   
+            playHowlSound(value);
+          };
+          const totalTime = (Date.now() - curTime);
+          await timeout(timePerBaMs - totalTime);
+        }
      };
+
+    const playAllLayers = async() => {
+        if (layers.length === 0) {return};
+        
+        let curTime = Date.now();
+
+        await Promise.all(layers.map(layer => playLayer(layer)));
+
+        const totalTime = Date.now() - curTime;
+        await timeout(4800 - totalTime)
+    }
   
 
     return (
         <div>  
-            <button onClick={playLoop}>NO WAY NO WAY</button>
+            <button onClick={playAllLayers}>NO WAY NO WAY</button>
+            <button onClick={testMe}>Test Me</button>
+            <button onClick={() => setLooping(!looping)}>{looping ? "Stop looping" : "Start Looping"}</button>
         </div>
     )
 }
--- src/components/Looper.tsx
+++ src/components/Looper.tsx
@@ -1,48 +1,72 @@
-import {useState} from "react"
-import { useSoundMapContext } from "@/contexts/mapContext"
-import { playHowlSound } from "@/app/utils/howlUtils/HowlUtils"
+import { playHowlSound } from "@/app/utils/howlUtils/HowlUtils";
+import { useSoundMapContext } from "@/contexts/mapContext";
+import { isHowl } from "@/app/utils/howlUtils/HowlUtils";
+import { isSpriteHowl } from "@/app/utils/howlUtils/HowlUtils";
+import { useState, useEffect } from "react";
+
 interface Beat {
     key: string,
     timeStamp: number
 }
 
-const Looper = () => {
-
+const KeyBoard = () => {
+    const soundMap = useSoundMapContext();
     const [beats, setBeat] = useState<Beat[]>([])
-    let soundMap = useSoundMapContext();
+    const [looping, setLooping] = useState<boolean>(false)
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyPress);
+    }, [])
+    
+
+    const timeout = (ms: number) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     const handleKeyPress = (e: KeyboardEvent) => {
         const keyPressed = e.key;
+
+        const soundMapValue = soundMap.soundMap.get(keyPressed);
         const currentTime = Date.now();
-        setBeat((prevBeat) => [...prevBeat, {key: keyPressed, timeStamp: currentTime}])
+        setBeat((prevBeat) => [...prevBeat, {key: keyPressed, timeStamp: currentTime}]);
+       
+        if (soundMapValue === undefined) {return};
    }
 
-   const playLoop = () => {
-    if (beats.length === 0) return;
 
-    let currentTime = Date.now();
-
-    beats.forEach((beat, index) => {
-      const delay = beat.timeStamp - (Date.now() - currentTime);
-      setTimeout(() => {
-        const value = soundMap.soundMap.get(beat.key);
-
-        if (value === undefined) {return};
-
-        playHowlSound(value);
-     
-      }, delay);
-    });
-  };
-
-
-    window.addEventListener("keydown", handleKeyPress);
+    const playLoop = async () => {
+       if (beats.length === 0) return;
+   
+       let prevTime = beats[0].timeStamp;
+       while (looping) {
+        let curTime = Date.now();
+        for (const beat of beats) {
+            const delay = beat.timeStamp - prevTime;
+            prevTime = beat.timeStamp;
+   
+            await timeout(delay);
+            console.log(delay)
+   
+            const value = soundMap.soundMap.get(beat.key);
+   
+            if (value === undefined) {return};
+   
+            playHowlSound(value);
+          };
+          const totalTime = (Date.now() - curTime);
+          await timeout(4800 - totalTime);
+        }
+     };
+  
+     //so we DO need a set tempo + how many bars we wanna record
+     // Then calculate time length by doing 60 / BPM * 8
 
     return (
         <div>  
-          <button onClick={playLoop}>No way this works</button>
+            <button onClick={playLoop}>NO WAY NO WAY</button>
+            <button onClick={() => setLooping(!looping)}>{looping ? "Stop looping" : "Start Looping"}</button>
         </div>
     )
 }
 
-export default Looper;
+export default KeyBoard;
